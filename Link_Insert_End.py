bc92fe7e04270893c6608780c73cc88c23827af1
Kavya Menon <kavyasmenon10@gmail.com>
2026-01-03 02:39:34 +0530

    Implement insertion operation at the tail of a linked list

---
+++ Link_Insert_End.py
@@ -0,0 +1,22 @@
+def insertNodeAtTail(head, data):
+
+    new_node = SinglyLinkedListNode(data)
+    if head is None:
+        return new_node
+    
+    tail = head
+    while tail.next:
+        tail = tail.next
+    tail.next = new_node
+    return head
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
