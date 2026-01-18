79427d70b01ac2aa7ab256ce16a609227a2b65f4
Kavya Menon <kavyasmenon10@gmail.com>
2026-01-18 23:45:53 +0530

    Implement basic Stack class with push and pop methods

---
+++ Stack.py
@@ -0,0 +1,13 @@
+class Stack():
+  def_init_(self):
+  self.container=deque()
+  def push(self,val):
+    self.container.append(val)
+  def pop(self):
+    return self.container.pop()
+  def peek(self):
+    return self.container[-1]
+  def is_empty(self):
+    return len(self.container=0)
+  def is_full(self):
+    return len(self.container)
