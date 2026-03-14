b5812d0b6d0bb59c07f622a4c823c29a35064247
Kavya Menon <kavyasmenon10@gmail.com>
2026-03-14 23:33:34 +0530

    Return data of the merged node
    Return the data of the merged node in the linked lists.

--- Find_Merged_Node.py
+++ Find_Merged_Node.py
@@ -3,3 +3,4 @@ def findMergeNode(head1, head2):
     while pointer1 != pointer2:
         pointer1 = pointer1.next if pointer1 else head2
         pointer2 = pointer2.next if pointer2 else head1
+    return pointer1.data  # or pointer2.data, both are same
